2f555121c7eecabbb347312fa82db54b6d99e5fe
Faizanahmedsy <saiyed.faizanahmed1@gmail.com>
2024-04-24 21:05:58 +0530

    new deisgn

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,475 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Wrapper from "@/components/wrapper";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
+import { title } from "process";
+
+interface Course {
+  title: string;
+  description?: string;
+  optional?: boolean;
+}
+
+interface Curriculum {
+  title: string;
+  courses: Array<Course>;
+}
 
 export default function Home() {
+  const majors = [
+    {
+      title: "Artificial Intelligence",
+    },
+    {
+      title: "Bio Technology",
+    },
+    {
+      title: "Cloud Computing",
+    },
+    {
+      title: "Cyber Security",
+    },
+    {
+      title: "Data Science",
+    },
+    {
+      title: "Game Development",
+    },
+    {
+      title: "Software Development",
+    },
+  ];
+
+  const minors = [
+    {
+      title: "Web Design",
+    },
+    {
+      title: "Economics",
+    },
+    {
+      title: "Finance",
+    },
+  ];
+
+  const curriculumData: Curriculum[] = [
+    {
+      title: "Term 1",
+      courses: [
+        {
+          title: "Fundamentals of Computers",
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.",
+        },
+        {
+          title: "Fundamentals of Programming",
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.",
+        },
+        {
+          title: "Maths for CS",
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.",
+        },
+      ],
+    },
+    {
+      title: "Term 2",
+      courses: [
+        {
+          title: "Java and Object-oriented programming",
+        },
+        {
+          title: "Data Structures and Algorithms",
+        },
+        {
+          title: "Problem Solving and Logical Thinking",
+        },
+        {
+          title: "Elective 1",
+          optional: true,
+        },
+      ],
+    },
+    {
+      title: "Term 3",
+      courses: [
+        {
+          title: "Database management systems",
+        },
+        {
+          title: "Communication Skills, Networking and Team Building",
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.",
+        },
+        {
+          title: "Internet 101",
+        },
+        {
+          title: "Elective 1",
+          optional: true,
+        },
+      ],
+    },
+    {
+      title: "Term 4",
+      courses: [
+        {
+          title: "Operating System",
+        },
+        {
+          title: "Data Structures and Algorithms 2",
+        },
+
+        {
+          title: "Elective 2",
+          optional: true,
+        },
+      ],
+    },
+    {
+      title: "Term 5",
+      courses: [
+        {
+          title: "Python and Machine Learning",
+        },
+        {
+          title: "Productivity and Work Efficiency",
+        },
+        {
+          title: "Data Structures and Algorithms 3",
+        },
+        {
+          title: "Elective 3",
+          optional: true,
+        },
+      ],
+    },
+
+    {
+      title: "Term 6",
+      courses: [
+        {
+          title: "Fundamentals of Web Development",
+        },
+        {
+          title: "Discrete mathematics",
+        },
+        {
+          title: "Data Structures and Algorithms 4",
+        },
+        {
+          title: "Elective 6",
+          optional: true,
+        },
+      ],
+    },
+    {
+      title: "Term 7",
+      courses: [
+        {
+          title: "System Design",
+        },
+
+        {
+          title: "Computer Networks",
+        },
+
+        {
+          title: "Elective 4",
+          optional: true,
+        },
+      ],
+    },
+    {
+      title: "Term 8",
+      courses: [
+        {
+          title: "Cloud Computing",
+        },
+        {
+          title: "Product Management",
+        },
+
+        {
+          title: "Elective 8",
+          optional: true,
+        },
+
+        {
+          title: "Elective 8",
+          optional: true,
+        },
+      ],
+    },
+    {
+      title: "Term 9",
+      courses: [
+        {
+          title: "Advance Java",
+        },
+        {
+          title: "Data Mining",
+        },
+        {
+          title: "Elective 6",
+          optional: true,
+        },
+        {
+          title: "Elective 6",
+          optional: true,
+        },
+      ],
+    },
+    {
+      title: "Term 10",
+      courses: [
+        {
+          title: "Software Engineering",
+        },
+        {
+          title: "Project",
+        },
+        {
+          title: "Elective 9",
+          optional: true,
+        },
+        {
+          title: "Elective 10",
+          optional: true,
+        },
+      ],
+    },
+    {
+      title: "Term 11",
+      courses: [
+        {
+          title: "Cyber Security",
+        },
+        {
+          title: "Project",
+        },
+        {
+          title: "Elective 11",
+          optional: true,
+        },
+        {
+          title: "Elective 12",
+          optional: true,
+        },
+      ],
+    },
+    {
+      title: "Term 12",
+      courses: [
+        {
+          title: "Project Management",
+        },
+        {
+          title: "Project",
+        },
+        {
+          title: "Elective 11",
+          optional: true,
+        },
+        {
+          title: "Elective 12",
+          optional: true,
+        },
+      ],
+    },
+  ];
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <Wrapper className="min-h-screen">
+        <nav>
+          <div>Neon School</div>
+          <div></div>
+        </nav>
+        <div className="flex justify-center items-center h-screen">
+          <div>Neon International School of technology</div>
         </div>
-      </div>
+      </Wrapper>
+      <Wrapper>
+        <div className="text-center">{`BTech in Computer Science with {major} and {minor}`}</div>
+      </Wrapper>
+      <Wrapper>
+        <div>Majors</div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <div className="grid grid-cols-3 gap-4 py-2">
+          {majors.map((major, index) => (
+            <Card key={index}>
+              <div className="p-4">{major.title}</div>
+            </Card>
+          ))}
+        </div>
+      </Wrapper>
+      <Wrapper>
+        <div>Minors</div>
+        <div className="grid grid-cols-3 gap-4 py-2">
+          {minors.map((major, index) => (
+            <Card key={index}>
+              <div className="p-4">{major.title}</div>
+            </Card>
+          ))}
+        </div>
+      </Wrapper>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <Wrapper>
+        <div className="text-3xl font-bold ">
+          <span className="border-b-4 border-orange-700">Curriculum</span>
+        </div>
+        <Card className="bg-blue-800 my-5">
+          <CardHeader>
+            <CardTitle className="text-blue-100">Freshman Year</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="grid grid-cols-3 gap-4">
+              {curriculumData.slice(0, 3).map((item, index) => (
+                <Card key={index}>
+                  <CardHeader>
+                    <CardTitle>{item.title}</CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    {item.courses.map((course, index) => (
+                      <div key={index} className="py-2">
+                        {/* <CardTitle>{course.title}</CardTitle>
+                    <CardDescription>{course.description}</CardDescription> */}
+                        <div className="flex gap-2">
+                          <div className="p-1">{index + 1}</div>
+                          <div
+                            className={cn(
+                              "font-medium bg-red-100 p-1 rounded-lg",
+                              course.optional && "bg-green-100"
+                            )}
+                          >
+                            <div>{course.title}</div>
+                          </div>
+                        </div>
+                      </div>
+                    ))}
+                  </CardContent>
+                </Card>
+              ))}
+            </div>
+          </CardContent>
+        </Card>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        <Card className="bg-green-800  my-5">
+          <CardHeader>
+            <CardTitle className="text-green-100">Sophomore Year</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="grid grid-cols-3 gap-4">
+              {curriculumData.slice(3, 6).map((item, index) => (
+                <Card key={index}>
+                  <CardHeader>
+                    <CardTitle>{item.title}</CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    {item.courses.map((course, index) => (
+                      <div key={index} className="py-2">
+                        {/* <CardTitle>{course.title}</CardTitle>
+                    <CardDescription>{course.description}</CardDescription> */}
+                        <div className="flex gap-2">
+                          <div className="p-1">{index + 1}</div>
+                          <div
+                            className={cn(
+                              "font-medium bg-red-100 p-1 rounded-lg",
+                              course.optional && "bg-green-100"
+                            )}
+                          >
+                            <div>{course.title}</div>
+                          </div>
+                        </div>
+                      </div>
+                    ))}
+                  </CardContent>
+                </Card>
+              ))}
+            </div>
+          </CardContent>
+        </Card>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+        <Card className="bg-yellow-800  my-5">
+          <CardHeader>
+            <CardTitle className="text-yellow-100">Junior Year</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="grid grid-cols-3 gap-4">
+              {curriculumData.slice(6, 9).map((item, index) => (
+                <Card key={index}>
+                  <CardHeader>
+                    <CardTitle>{item.title}</CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    {item.courses.map((course, index) => (
+                      <div key={index} className="py-2">
+                        {/* <CardTitle>{course.title}</CardTitle>
+                    <CardDescription>{course.description}</CardDescription> */}
+                        <div className="flex gap-2">
+                          <div className="p-1">{index + 1}</div>
+                          <div
+                            className={cn(
+                              "font-medium bg-red-100 p-1 rounded-lg",
+                              course.optional && "bg-green-100"
+                            )}
+                          >
+                            <div>{course.title}</div>
+                          </div>
+                        </div>
+                      </div>
+                    ))}
+                  </CardContent>
+                </Card>
+              ))}
+            </div>
+          </CardContent>
+        </Card>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <Card className="bg-rose-800  my-5">
+          <CardHeader>
+            <CardTitle className="text-rose-100">Senior Year</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="grid grid-cols-3 gap-4">
+              {curriculumData.slice(9, 12).map((item, index) => (
+                <Card key={index}>
+                  <CardHeader>
+                    <CardTitle>{item.title}</CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    {item.courses.map((course, index) => (
+                      <div key={index} className="py-2">
+                        {/* <CardTitle>{course.title}</CardTitle>
+                    <CardDescription>{course.description}</CardDescription> */}
+                        <div className="flex gap-2">
+                          <div className="p-1">{index + 1}</div>
+                          <div
+                            className={cn(
+                              "font-medium bg-red-100 p-1 rounded-lg",
+                              course.optional && "bg-green-100"
+                            )}
+                          >
+                            <div>{course.title}</div>
+                          </div>
+                        </div>
+                      </div>
+                    ))}
+                  </CardContent>
+                </Card>
+              ))}
+            </div>
+          </CardContent>
+        </Card>
+      </Wrapper>
+    </>
   );
 }
